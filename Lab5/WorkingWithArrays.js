8d3255611b0e0a0e62936e0bbc55bbf0b7b6bdbe
Sai Swapnil Aremanda <aremanda.s@northeastern.edu>
2024-07-17 23:39:08 +0530

    minor fix

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -16,6 +16,16 @@ export default function WorkingWithArrays(app) {
     res.json(todos);
   });
 
+  app.get("/lab5/todos/create", (req, res) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      title: "New Task",
+      completed: false,
+    };
+    todos.push(newTodo);
+    res.json(todos);
+  });
+
   app.get("/lab5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
@@ -62,7 +72,6 @@ export default function WorkingWithArrays(app) {
       res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
       return;
     }
-
     todos.splice(todoIndex, 1);
     res.sendStatus(200);
   });
