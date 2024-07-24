fd24e6fa80a6cb8b57246a1f73f36a3cd2063bc8
Sai Swapnil Aremanda <aremanda.s@northeastern.edu>
2024-07-24 20:31:57 +0530

    a6 initial

--- App.js
+++ App.js
@@ -5,12 +5,21 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
+
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
