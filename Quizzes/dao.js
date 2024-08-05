8adee3df4195a5ff9bd36fac9d65858325380d0f
Sai Swapnil Aremanda <aremanda.s@northeastern.edu>
2024-08-05 23:23:02 +0530

    project commit for quiz list and details screen

--- Quizzes/dao.js
+++ Quizzes/dao.js
@@ -5,7 +5,18 @@ export const createQuiz = (quiz) => model.create(quiz);
 export const findAllQuizzes = (courseId) => model.find({ courseId: courseId });
 
 export const findQuizById = (courseId, quizId) =>
-  model.find({ courseId: courseId, _id: quizId });
+  model.findOne({ courseId: courseId, _id: quizId });
 
 export const updateQuiz = (courseId, quizId, quiz) =>
   model.updateOne({ courseId: courseId, _id: quizId }, quiz);
+
+export const deleteQuiz = (courseId, quizId) =>
+  model.deleteOne({ courseId, _id: quizId });
+
+export const findQuizzesByPartialName = (courseId, partialName) => {
+  const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+  return model.find({
+    courseId,
+    $or: [{ title: { $regex: regex } }],
+  });
+};
--- Quizzes/routes.js
+++ Quizzes/routes.js
@@ -16,6 +16,7 @@ export default function QuizRoutes(app) {
   const findQuizById = async (req, res) => {
     const { courseId, quizId } = req.params;
     const quiz = await dao.findQuizById(courseId, quizId);
+    // console.log(quiz);
     res.json(quiz);
   };
 
@@ -25,8 +26,25 @@ export default function QuizRoutes(app) {
     res.json(status);
   };
 
+  const deleteQuiz = async (req, res) => {
+    const { courseId, quizId } = req.params;
+    const status = await dao.deleteQuiz(courseId, quizId);
+    res.send(status);
+  };
+
+  const findQuizzesByPartialName = async (req, res) => {
+    const { courseId, partialName } = req.params;
+    const quizzes = await dao.findQuizzesByPartialName(courseId, partialName);
+    res.json(quizzes);
+  };
+
   app.post("/api/quizzes/:courseId", createQuiz);
   app.get("/api/quizzes/:courseId", findAllQuizzes);
   app.get("/api/quizzes/:courseId/:quizId", findQuizById);
   app.put("/api/quizzes/:courseId/:quizId", updateQuiz);
+  app.delete("/api/quizzes/:courseId/:quizId", deleteQuiz);
+  app.get(
+    "/api/quizzes/:courseId/search/:partialName",
+    findQuizzesByPartialName
+  );
 }
