f2457c24d4396b6d5283130022af4161b0a1fbbe
Samarth <samarthgalchar@gmail.com>
2024-08-08 14:26:59 -0400

    reworked schema

--- Quizzes/schema.js
+++ Quizzes/schema.js
@@ -1,48 +1,55 @@
 import mongoose from "mongoose";
-const quizSchema = new mongoose.Schema(
-  {
-    courseId: String,
-    numQuestions: Number,
-    published: Boolean,
-    title: String,
-    description: String,
-    quizType: String,
-    points: Number,
-    group: String,
-    shuffle: Boolean,
-    hasTimeLimit: Number,
-    timeLimit: Number,
-    multipleAttempt: Boolean,
-    showCorrect: Boolean,
-    accessCode: String,
-    oneQView: Boolean,
-    webcamReq: Boolean,
-    lockAfterAttempt: Boolean,
-    dueDate: Date,
-    availableFrom: Date,
-    availableUntil: Date,
-    questions: [
-      {
-        id: String,
-        type: String,
-        points: Number,
-        text: String,
-        choices: [
-          {
-            id: Number,
-            text: String,
-            isCorrect: Boolean,
-          }
-        ],
-        blanks: [{
-          id: Number,
-          text: String
-        }],
-        answer: Boolean,
-    }
-    ]
 
-  },
-  { collection: "quizzes" }
-);
+const questionSchema = new mongoose.Schema({
+  id: String,
+  type: String,
+  points: Number,
+  text: String,
+  choices: [
+    {
+      id: Number,
+      text: String,
+      isCorrect: Boolean
+    }
+  ],
+  blanks: [
+    {
+      id: Number,
+      text: String
+    }
+  ],
+  answer: Boolean
+}, 
+{
+  _id: false // Prevent Mongoose from creating a new _id for each question
+});
+
+const quizSchema = new mongoose.Schema({
+  courseId: String,
+  numQuestions: Number,
+  published: Boolean,
+  title: String,
+  description: String,
+  quizType: String,
+  points: Number,
+  group: String,
+  shuffle: Boolean,
+  hasTimeLimit: Boolean,
+  timeLimit: Number,
+  multipleAttempt: Boolean,
+  showCorrect: Boolean,
+  accessCode: String,
+  oneQView: Boolean,
+  webcamReq: Boolean,
+  lockAfterAttempt: Boolean,
+  dueDate: Date,
+  availableFrom: Date,
+  availableUntil: Date,
+  questions: [questionSchema]
+}, 
+{
+  collection: "quizzes",
+  timestamps: true // Optional: add createdAt and updatedAt fields
+});
+
 export default quizSchema;
